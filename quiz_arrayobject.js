aeea602fb5da3d3bf1c39e332999567208a303e0
zedricka <zedrick.ara@gmail.com>
2016-12-07 08:30:52 -1000

    wow

---
+++ quiz_arrayobject.js
@@ -0,0 +1,45 @@
+/* 
+
+  Zedrick Ara
+  December 7, 2016
+  Period 1
+  Coding
+
+*/
+
+
+// My Array
+
+var myArray = ["Chocolate", "Pineapple", 10, true];
+
+console.log (myArray[2]);
+
+function ahhhh (boop) {
+  if (boop == true) {
+    console.log (myArray[0] + " is actually " + myArray[2] + " " + myArray[1]);
+  }
+  else {
+    console.log ("You Shall Not Pass!");
+  }
+}
+ahhhh (myArray[3]);
+
+
+//Batman
+
+var batman {
+  Man : true,
+  utilityBelt : ["Pokeball", "Batarang", "Taco"],
+  vehicle : "batmobile",
+  sidekick : "robin",
+
+function yes (what) {
+  if (what == true) {
+    console.log ("The Dark Knight shall purge Gothem City with his " + batman.utilityBelt[1] + " " + batman.vehicle + " and " + batman.sidekick + ".");
+  }
+  else {
+    console.log ("No, I'm the Bat.");
+  }
+}
+}
+yes (batman.Man);
